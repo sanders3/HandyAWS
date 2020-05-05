79d4e5afaa7f398d639b5d4a9db9d10fb0efe3ac
Mark Sanders <mark.a.sanders@capgemini.com>
2020-05-05 14:44:52 +0100

    Add JSON support
    - test file handy-tool-catalog.json
    - Product pojo and test

---
+++ src/main/java/com/handy/aws/functions/Product.java
@@ -0,0 +1,107 @@
+package com.handy.aws.functions;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class Product {
+
+	private int id;
+	private String toolType;
+	private String brand;
+	private String name;
+	private int count;
+
+	public Product() {
+		this.id = -1;
+	}
+	
+	public Product(int id, String toolType, String brand, String name, int count) {
+		this.id = id;
+		this.toolType = toolType;
+		this.brand = brand;
+		this.name = name;
+		this.count = count;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getToolType() {
+		return toolType;
+	}
+
+	public void setToolType(String toolType) {
+		this.toolType = toolType;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+				.append(this.id)
+				.append(this.toolType)
+				.append(this.brand)
+				.append(this.name)
+				.append(this.count)
+				.toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (obj == null) {
+			return false;
+		}
+
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		Product other = (Product) obj;
+		return new EqualsBuilder()
+				.append(this.id, other.id)
+				.append(this.toolType, other.toolType)
+				.append(this.brand, other.brand)
+				.append(this.name, other.name)
+				.append(this.count, other.count)
+				.isEquals();
+	}
+	
+	@Override
+	public String toString() {
+		return "Product ["
+				+ "id=" + id + ", toolType=" + toolType + ", brand=" + brand + ", name=" + name + ", count=" + count 
+				+ "]";
+	}
+}
---
+++ src/test/java/com/handy/aws/functions/ProductTest.java
@@ -0,0 +1,39 @@
+package com.handy.aws.functions;
+
+import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class ProductTest {
+
+	@Test
+	public void pojoWellImplemented() {
+	    assertPojoMethodsFor(Product.class).areWellImplemented();
+	}
+
+	private static final String JSON = "{" 
+				+ "\"id\":100," 
+				+ "\"toolType\":\"Hammer\"," 
+				+ "\"brand\":\"Stanley\"," 
+				+ "\"name\":\"5oz Magnetic Tack Hammer\"," 
+				+ "\"count\":20" 
+				+ "}";
+
+	@Test
+	public void unmarshal() {
+		Product actual = new Gson().fromJson(JSON, Product.class);
+		Product expected = new Product(100, "Hammer", "Stanley", "5oz Magnetic Tack Hammer", 20);
+		assertThat(actual, is(expected));
+	}
+
+	@Test
+	public void marshal() {
+		Product product = new Product(100, "Hammer", "Stanley", "5oz Magnetic Tack Hammer", 20);
+		String actual = new Gson().toJson(product);
+		assertThat(actual, is(JSON));
+	}
+}
